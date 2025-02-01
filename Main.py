63f1cb191fce37e1e75c82c2c8e4f5809377a10f
WhaleRed <redwhalecs@gmail.com>
2025-02-01 19:15:00 +0800

    Added add student Entry fields

--- Main.py
+++ Main.py
@@ -1,12 +1,16 @@
 from tkinter import *
 import csv
 
-sort_name = "None"
+student_list = []
+program_list = []
+college_list = []
+
+student_path = "StudentData.csv"
+program_path = "ProgramData.csv"
+college_path = "CollegeData.csv"
 
 def sort_year_level():
   print("Sorting by Year level")
-  global sort_name
-  sort_name = "Year level"
 
 def sort_gender():
   print("Sorting by gender")
@@ -20,32 +24,75 @@ def sort_college():
 def sort_age():
   print("Sorting by age")
 
+def submit_add():
+  print("Submitted the newly added student")
+
 def add_student():
-  print("Adding student")
+  add_window = Toplevel()
+  add_window.title("Add Student")
+
+  Label(add_window, text="STUDENT", font=('Helvetica', 20)).grid(row=0, column=0, columnspan=2, pady=10)
+  Label(add_window, text="ID#: ", font=('Helvetica', 15)).grid(row=1, column=0, padx=10)
+  Entry(add_window,font=('Helvetica', 15)).grid(row=1, column=1, padx=10)
+  Label(add_window, text="First name: ", font=('Helvetica', 15)).grid(row=2, column=0,)
+  Entry(add_window,font=('Helvetica', 15)).grid(row=2, column=1,)
+  Label(add_window, text="Last name: ", font=('Helvetica', 15)).grid(row=3, column=0,)
+  Entry(add_window,font=('Helvetica', 15)).grid(row=3, column=1,)
+  Label(add_window, text="Year Level: ", font=('Helvetica', 15)).grid(row=4, column=0,)
+  Entry(add_window,font=('Helvetica', 15)).grid(row=4, column=1,)
+  Label(add_window, text="Gender: ", font=('Helvetica', 15)).grid(row=5, column=0,)
+  Entry(add_window,font=('Helvetica', 15)).grid(row=5, column=1,)
+  Label(add_window, text="Program Code: ", font=('Helvetica', 15)).grid(row=6, column=0,)
+  Entry(add_window,font=('Helvetica', 15)).grid(row=6, column=1,)
+
+  Label(add_window, text="PROGRAM", font=('Helvetica', 20)).grid(row=7, column=0, columnspan=2, pady=10)
+  Label(add_window, text="Program Code: ", font=('Helvetica', 15)).grid(row=8, column=0,)
+  Entry(add_window,font=('Helvetica', 15)).grid(row=8, column=1,)
+  Label(add_window, text="Name: ", font=('Helvetica', 15)).grid(row=9, column=0,)
+  Entry(add_window,font=('Helvetica', 15)).grid(row=9, column=1,)
+  Label(add_window, text="College: ", font=('Helvetica', 15)).grid(row=10, column=0,)
+  Entry(add_window,font=('Helvetica', 15)).grid(row=10, column=1,)
+
+  Label(add_window, text="College", font=('Helvetica', 20)).grid(row=11, column=0, columnspan=2, pady=10)
+  Label(add_window, text="Code: ", font=('Helvetica', 15)).grid(row=12, column=0,)
+  Entry(add_window,font=('Helvetica', 15)).grid(row=12, column=1,)
+  Label(add_window, text="Name: ", font=('Helvetica', 15)).grid(row=13, column=0,)
+  Entry(add_window,font=('Helvetica', 15)).grid(row=13, column=1,)
+
+  Button(add_window, text="Add Student", font=('Helvetica', 15), command=submit_add).grid(row=14, column=0, columnspan=2, pady=10)
+
 
 def delete_student():
   print("Deleting student")
 
-#Updates list
-def update(Student):
-  #clear
-  student_list.delete(0, END)
 
-  #Add students
-  for student in Student:
-    student_list.insert(END, student)
+#Show list
+def show():
+  with open(student_path, "r") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        student_list.insert("end", row)
+
+#Updates list for search
+def update(students):
+  for student in students:
+    student_list.insert(student)
+
+    
 
 #Checks if searched is on the list and updates the list
-def search(event):
-  searched = search_box.get()
-  if searched == '':
-    student = names
-  else:
-    student = []
-    for item in names:
-      if searched.lower() in item.lower():
-        student.append(item)
-  update(student)
+#def search(event):
+# searched = search_box.get()
+# with open(student_path, "r") as f:
+#   reader = csv.reader(f)
+# if searched == '':
+#    student = reader
+# else:
+#    student = []
+#    for item in reader:
+#      if searched.lower() in item.lower():
+#        student.append(item)
+# update(student)
 
 main_window = Tk()
 
@@ -87,12 +134,9 @@ Label(search_frame, text="Search by ID#").pack(side=RIGHT)
 student_list = Listbox(main_window, width=300, height=300, font=("Helvetica", 15))
 student_list.pack(pady=50, padx=20)
 
-#Test students, to be deleted
-names = ["Dababy", "Cench", "Bruno", "50", "Slim", "Snoop", "Dre"]
+show()
 
-update(names)
-
-search_box.bind("<KeyRelease>", search)
+#search_box.bind("<KeyRelease>", search)
 
 
 main_window.mainloop()
